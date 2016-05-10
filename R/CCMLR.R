ae8943a6e48886ca3858aa5b9b0548c683d5571e
Enrico Anello <enrico.anello@gmail.com>
2016-05-10 15:46:16 +0200

    Update CCMLR.R

--- R/CCMLR.R
+++ R/CCMLR.R
@@ -213,6 +213,7 @@ FishingDays <- function(start=1946, end=2016, species=c(), gear=c(), asd=c(), mo
     aggr02[[SpeciesCode]] <<- as.numeric(unlist(lista));
   })
   json <- toJSON(aggr02)
+  print(aggr02)
   aggr03 <- setDT(aggr02)[, lapply(.SD, sum), by=.(monthNum), .SDcols=c("KRI")]
   aggr03$out <- paste(aggr03$Year, aggr03$monthNum, sep="/")
 
