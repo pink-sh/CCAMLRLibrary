0d9795ae0dbb40b1b5875a2d34c99a00bf3e5b38
Enrico Anello <enrico.anello@gmail.com>
2016-05-10 15:44:00 +0200

    Update CCMLR.R

--- R/CCMLR.R
+++ R/CCMLR.R
@@ -213,7 +213,7 @@ FishingDays <- function(start=1946, end=2016, species=c(), gear=c(), asd=c(), mo
     aggr02[[SpeciesCode]] <<- as.numeric(unlist(lista));
   })
   json <- toJSON(aggr02)
-  aggr03 <- setDT(aggr02)[, lapply(.SD, sum), by=.(Year, Month, monthNum), .SDcols=species]
+  aggr03 <- setDT(aggr02)[, lapply(.SD, sum), by=.(monthNum), .SDcols=species]
   aggr03$out <- paste(aggr03$Year, aggr03$monthNum, sep="/")
 
   m1 <- mPlot(x = c("out"), y = species, type = chart, data = aggr03, stacked = "TRUE", xLabelAngle = 65)
