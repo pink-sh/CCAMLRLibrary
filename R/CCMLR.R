6f8d3161fefc8ed0f922baca68b8431a9400b156
Enrico Anello <enrico.anello@gmail.com>
2016-05-10 16:04:21 +0200

    Update CCMLR.R

--- R/CCMLR.R
+++ R/CCMLR.R
@@ -1,3 +1,4 @@
+@import data.table
 vector.is.empty <- function(x) return(length(x) ==0 )
 
 plotQuantitiesInTonnes <- function(species=c(), start=1946, end=2016, file="qryTable02.csv", chart="Bar") {
@@ -157,7 +158,6 @@ catchByFishingMethods <- function(start=1946, end=2016, species=c(), gear=c(), a
 }
 
 FishingDays <- function(start=1946, end=2016, species=c(), gear=c(), asd=c(), months=c(), file="qryTable11_5.csv", chart="Bar", removeZero=FALSE) {
-  import(data.table)
   library(rCharts)
   library(dplyr)
   library(jsonlite)
